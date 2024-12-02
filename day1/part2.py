c48b68de74b27be508fb205681e586930eb26509
Kubz <jak.przybylinski@gmail.com>
2024-12-02 13:14:48 +0100

    init

--- day1/part2.py
+++ day1/part2.py
@@ -7,3 +7,16 @@ file.close()
 #split data into two arrays
 left = []
 right = []
+
+for row in data:
+    #strip row and split into columns
+    row = row.strip().split('   ')
+
+    #to make sorting easier parse value to int
+    left.append(int(row[0]))
+    right.append(int(row[1]))
+
+#sort arrays in ascending order
+left.sort()
+right.sort()
+
