bbaf77f2df060aeb07a73837ee7a993a1f78c7fa
Kubz <jak.przybylinski@gmail.com>
2024-12-02 13:52:48 +0100

    get all increasing and decreasing rows

--- day2/part1.py
+++ day2/part1.py
@@ -3,3 +3,15 @@ file = open('data.txt','r')
 data = file.readlines()
 file.close()
 
+partSafe = []
+#remove rows that aren't all increasing or all decreasing
+for row in data:
+    row = row.strip().split(' ')
+    #check if all increases
+    if sorted(row) == row:
+        partSafe.append(row)
+    #check if all decreasing
+    elif sorted(row, reverse=True) == row:
+        partSafe.append(row)
+
+
