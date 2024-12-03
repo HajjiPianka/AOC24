87df0b1a728933631d172be984a05eb27f69c17e
Jakub Przybylinski <jak.przybylinski@gmail.com>
2024-12-03 04:28:36 +0100

    revert changes to smallDifference and reuse bad rows

--- day2/part2.py
+++ day2/part2.py
@@ -3,6 +3,7 @@ file = open('data.txt','r')
 data = file.readlines()
 file.close()
 
+unsafeRows = []
 partSafe = []
 #remove rows that aren't all increasing or all decreasing
 for row in data:
@@ -13,24 +14,22 @@ for row in data:
     #check if all decreasing
     elif sorted(row, reverse=True) == row:
         partSafe.append(row)
+    else:
+        unsafeRows.append(row)
 
 def smallDifferenceWithOutOneLevel(row: list) -> bool:
     '''check if removing a single level fixes the row'''
-    for removedLevelIndex in range(len(row)): # remove a single level
-        newRow = row[:removedLevelIndex] + [removedLevelIndex + 1]
-
+    ...
 
 #check difference between elements
 def smallDifference(row: list) -> bool:
     '''check if step between values is between 1 and 3'''
-    invalid = False
+
     #compare value with the next one
     for i in range(len(row)-1):
         diff = abs(row[i] - row[i+1]) #absolute value in case of increasing 
         if diff == 0 or diff > 3: #if slope too high or values equal
-            invalid = True
-    if invalid: # test if modifying row changes the result
-        return smallDifferenceWithOutOneLevel(row)
+            return False
     return True
 
 #count safe rows
@@ -38,5 +37,7 @@ safeRows = []
 for row in partSafe:
     if smallDifference(row):
         safeRows.append(row)
+    else:
+        unsafeRows.append(row)
 
 print(len(safeRows))
