758998a1b28dbe6fd1b122f7f53818a1efc0c75e
Jakub Przybylinski <jak.przybylinski@gmail.com>
2024-12-13 12:52:25 +0100

    normal and reversed in line

--- day4/part1.py
+++ day4/part1.py
@@ -2,3 +2,14 @@ file = open('data.txt','r')
 data = file.readlines()
 file.close()
 
+normal = 0
+reverse = 0 
+
+for line in data:
+    line = line.strip()
+    for i in range(len(line)-3):
+        word = line[i] + line[i+1] + line[i+2] + line[i+3]
+        if word == 'XMAS':
+            normal += 1
+        elif word == 'SAMX':
+            reverse += 1
