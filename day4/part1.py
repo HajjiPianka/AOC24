7d141bc3a99992c0cef50d8d28dc059c7e635a0f
Jakub Przybylinski <jak.przybylinski@gmail.com>
2024-12-13 12:54:02 +0100

    cleanup

--- day4/part1.py
+++ day4/part1.py
@@ -2,14 +2,11 @@ file = open('data.txt','r')
 data = file.readlines()
 file.close()
 
-normal = 0
-reverse = 0 
+answer = 0
 
 for line in data:
     line = line.strip()
-    for i in range(len(line)-3):
+    for i in range(len(line)-3): #normal and reversed in line
         word = line[i] + line[i+1] + line[i+2] + line[i+3]
-        if word == 'XMAS':
-            normal += 1
-        elif word == 'SAMX':
-            reverse += 1
+        if word == 'XMAS' or word == 'SAMX':
+            answer += 1
