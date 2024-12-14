65197807837def5412431bc86ac2aef318b2b8cc
Jakub Przybylinski <jak.przybylinski@gmail.com>
2024-12-14 11:27:51 +0100

    part2

--- day4/part2.py
+++ day4/part2.py
@@ -3,3 +3,14 @@ data = file.readlines()
 file.close()
 
 answer = 0
+
+for i in range(1,len(data)-1): #skip first and last row, looking just middle ones (letter A)
+    line = data[i].strip()
+    for index in range(1,len(line)-1): #skip first and last letter to not get out of range
+        char = line[index]
+        if char == 'A':
+            chars = [data[i-1][index-1], data[i-1][index+1], data[i+1][index-1], data[i+1][index+1]] #check chars diagonal to 'A'
+            if chars.count('M') == 2 and chars.count('S') == 2:
+                answer += 1 
+                
+print(answer)
