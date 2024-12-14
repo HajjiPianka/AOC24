67b1e44e85b229de75e5ea8e5c7d407bc328fae6
Jakub Przybylinski <jak.przybylinski@gmail.com>
2024-12-14 12:38:49 +0100

    extract updates

--- day5/part1.py
+++ day5/part1.py
@@ -2,10 +2,14 @@ file = open('data.txt','r')
 data = file.readlines()
 file.close()
 rules = []
+updates = []
 for row in data:
     row = row.strip()
     if row.count('|'): #its a rule
         values = row.split('|')
         rules.append(values)
+    elif row == '': #skip empty rows
+        continue
     else: #updates
-        ...
+        values = row.split(',')
+        updates.append(values)
