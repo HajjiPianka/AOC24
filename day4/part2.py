a15eee3668540f32328baad1b0f684a3cdaebc91
Jakub Przybylinski <jak.przybylinski@gmail.com>
2024-12-14 12:11:30 +0100

    reverting overthinking the problem

--- day4/part2.py
+++ day4/part2.py
@@ -9,8 +9,10 @@ for i in range(1,len(data)-1): #skip first and last row, looking just middle one
     for index in range(1,len(line)-1): #skip first and last letter to not get out of range
         char = line[index]
         if char == 'A':
-            chars = [data[i-1][index-1], data[i-1][index+1], data[i+1][index-1], data[i+1][index+1]] #check chars diagonal to 'A'
-            if chars.count('M') == 2 and chars.count('S') == 2:
-                answer += 1 
+            diag1 = data[i-1][index-1] + 'A' + data[i+1][index+1] #first diagonal
+            diag2 = data[i-1][index+1] + 'A' + data[i+1][index-1] #second diagonal
+            if diag1 == 'MAS' or diag1 == 'SAM':
+                if diag2 == 'MAS' or diag2 == 'SAM':
+                    answer += 1
                 
 print(answer)
