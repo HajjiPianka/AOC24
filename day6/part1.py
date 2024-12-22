42ffa769f00c6e3b80a9b6ad396e15d9fa860399
Jakub Przybylinski <jak.przybylinski@gmail.com>
2024-12-22 23:34:27 +0100

    obstacle checking function

--- day6/part1.py
+++ day6/part1.py
@@ -1,4 +1,24 @@
 file = open('data.txt','r')
-data = file.readlines()
+map = file.readlines()
 file.close()
 
+visitedCount = 0
+x, y = 73, 94 #starting position
+
+moves = ['^', '>', 'v', '<']
+
+def isBlockadeAhead(currX, currY, facing):
+    checkX, checkY = currX, currY
+    #where to look
+    if facing == '<':
+        checkX -= 1
+    elif facing == '>':
+        checkX += 1
+    elif facing == 'v':
+        checkY += 1
+    else:
+        checkY -= 1
+    
+    if map[checkX][checkY] == '#':
+        return True
+    return False
